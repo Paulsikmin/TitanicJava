bd27705bd3c6c279953df5ab705c4e12e4c46e2c
paulsikmin <minmediaservice@gmail.com>
2023-07-11 16:22:24 +0900

    [FEAT] 23/07/11 음악관리프로그램 정렬기능 추가
    1. 정렬 알고리즘을 이용한 정렬기능 추가
     - 버블정렬
     - 삽입정렬
     - 선택정렬
    2. 문자열끼리 비교시 compareTo() 메소드 사용
     - 0이면 동일, 양수값이면 왼쪽이 큼, 음수값이면 왼쪽이 작음.

--- src/com/kh/day12/music/controller/MusicController.java
+++ src/com/kh/day12/music/controller/MusicController.java
@@ -83,6 +83,100 @@ public class MusicController {
 		}
 		return -1;
 	}
+	
+	public void insertionSortByTitleASC() {
+		// 삽입정렬 연습
+	}
+	
+	public void selectionSortByTitleASC() {
+		// 선택정렬 연습
+	}
+	
+	public void bubbleSortByTitleASC() {
+		// TODO Auto-generated method stub
+//		int [] nums; -> mList로 변경 필요
+		for(int i = 0; i < mList.size()-1; i++) {
+			for(int j = 0; j < (mList.size()-1)-i; j++) {
+				Music mOne = mList.get(j);
+				Music mTwo = mList.get(j+1);
+				// The operator > is undefined for the argument type(s) 
+				// java.lang.String, java.lang.String
+				// 결과값이 
+				// 0이면 동일함
+				// 양수값이면 왼쪽이 순서가 더 큼
+				// 음수값이면 왼쪽이 순서가 작음
+				if(mOne.getTitle().compareTo(mTwo.getTitle()) > 0) {
+					Music temp = mOne;
+					mList.set(j, mTwo);
+					mList.set(j+1, temp);
+				}
+			}
+		}
+	}
+	public void bubbleSortByTitleDESC() {
+		// TODO Auto-generated method stub
+//		int [] nums; -> mList로 변경 필요
+		for(int i = 0; i < mList.size()-1; i++) {
+			for(int j = 0; j < (mList.size()-1)-i; j++) {
+				Music mOne = mList.get(j);
+				Music mTwo = mList.get(j+1);
+				// The operator > is undefined for the argument type(s) 
+				// java.lang.String, java.lang.String
+				// 결과값이 
+				// 0이면 동일함
+				// 양수값이면 왼쪽이 순서가 더 큼
+				// 음수값이면 왼쪽이 순서가 작음
+				if(mOne.getTitle().compareTo(mTwo.getTitle()) < 0) {
+					Music temp = mOne;
+					mList.set(j, mTwo);
+					mList.set(j+1, temp);
+				}
+			}
+		}
+	}
+	
+	public void bubbleSortBySingerASC() {
+		// TODO Auto-generated method stub
+//		int [] nums; -> mList로 변경 필요
+		for(int i = 0; i < mList.size()-1; i++) {
+			for(int j = 0; j < (mList.size()-1)-i; j++) {
+				Music mOne = mList.get(j);
+				Music mTwo = mList.get(j+1);
+				// The operator > is undefined for the argument type(s) 
+				// java.lang.String, java.lang.String
+				// 결과값이 
+				// 0이면 동일함
+				// 양수값이면 왼쪽이 순서가 더 큼
+				// 음수값이면 왼쪽이 순서가 작음
+				if(mOne.getSinger().compareTo(mTwo.getSinger()) > 0) {
+					Music temp = mOne;
+					mList.set(j, mTwo);
+					mList.set(j+1, temp);
+				}
+			}
+		}
+	}
+	public void bubbleSortBySingerDESC() {
+		// TODO Auto-generated method stub
+//		int [] nums; -> mList로 변경 필요
+		for(int i = 0; i < mList.size()-1; i++) {
+			for(int j = 0; j < (mList.size()-1)-i; j++) {
+				Music mOne = mList.get(j);
+				Music mTwo = mList.get(j+1);
+				// The operator > is undefined for the argument type(s) 
+				// java.lang.String, java.lang.String
+				// 결과값이 
+				// 0이면 동일함
+				// 양수값이면 왼쪽이 순서가 더 큼
+				// 음수값이면 왼쪽이 순서가 작음
+				if(mOne.getSinger().compareTo(mTwo.getSinger()) < 0) {
+					Music temp = mOne;
+					mList.set(j, mTwo);
+					mList.set(j+1, temp);
+				}
+			}
+		}
+	}
 }
 
 
